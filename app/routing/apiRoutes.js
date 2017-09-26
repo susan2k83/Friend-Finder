fc9d8fa8d47c271352ece1baa68ff1ea6dab7d78
susan <susan2k83@yahoo.com>
2017-09-25 22:08:02 -0700

    final commit for HW

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,23 +8,39 @@ module.exports = function(app) {
     res.json(friendsData);
   });
 
-  
+//        the code between the lines has been commented out because it crashed the app:
+  // ________________________________________________________________________________
 
 //   Used to handle incoming survey results. Also used to handle the compatibiltiy logic
   // app.post("/api/friends", function(req, res) {
     
-  //     var bestMatch = {
-  //       name: "",
-  //       photo: "",
-  //       friendsDifference: 1000
-  //     }
+    //   var bestMatch = {
+    //     name: "",
+    //     photo: "",
+    //     friendsDifference: 1000
+    //   }
    
-  //   var userData = req.body;
-    
-  //   var userScore = userData.userScores;
+    // ------- the code stops here: "RefrenceError: req is not defined"-------
 
-  //   var totalDifference = 0;
-  // });
+    // var userData = req.body;
+   
+    
+    // var userScore = userData.scores;
+
+    //   var userScore = [];
+    //   var friends = [];
+    //   var differenceArr = [];
+    //   var totalDifference = 0;
+
+    //   for(var i = 0; i < userScore; i++) {
+        
+    //       totalDifference += (parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+    //       differenceArr.push(Math.abs(totalDifference));
+    //     }
+    //   console.log(differenceArr);
+      
+  // ________________________________________________________________________________
 
    // Here we loop through all the friend possibilities in the database.
 
